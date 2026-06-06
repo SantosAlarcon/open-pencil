7d27cac5ab7e296e7f28608f30b4cd9aa4fbb1c9
Danila Poyarkov <dev@dannote.net>
2026-06-06 13:42:48 +0300

    refactor(kiwi): remove redundant core shims

--- tests/engine/io/fig/roundtrip/glyph-blob.test.ts
+++ tests/engine/io/fig/roundtrip/glyph-blob.test.ts
@@ -5,7 +5,7 @@ import { resolve } from 'node:path'
 import { exportFigFile, initCodec, parseFigFile, SceneGraph } from '@open-pencil/core'
 import { fontManager } from '@open-pencil/core/text'
 
-import { parseFigBuffer } from '#core/kiwi/fig/parse/core'
+import { parseFigBuffer } from '@open-pencil/kiwi/fig/parse'
 
 import { HEAVY_TEST_TIMEOUT_MS } from '#tests/helpers/test-utils'
 
